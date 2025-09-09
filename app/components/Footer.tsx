4486b6b7bc2435f3e4d309d081227b886dd2105f
clavernet <jvergarati@ucvvirtual.edu.pe>
2025-09-08 22:59:18 -0500

    Update contact information in Footer component for accuracy

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -47,21 +47,21 @@ const Footer = () => {
                 <svg className="w-5 h-5 text-yellow-400 mt-1 mr-3 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
                 </svg>
-                <span className="text-blue-200 text-sm">Av. Julio C. Tello 1438 Urb. Risso - Lince, Lima</span>
+                <span className="text-blue-200 text-sm">Jirón Bolívar 350, Trujillo 13001, Perú</span>
               </div>
               
               <div className="flex items-center justify-center md:justify-start">
                 <svg className="w-5 h-5 text-yellow-400 mr-3 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
                   <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                 </svg>
-                <span className="text-blue-200 text-sm">+51 965 403 101 - Área Comercial</span>
+                <span className="text-blue-200 text-sm">+51 999 888 777</span>
               </div>
               
               <div className="flex items-center justify-center md:justify-start">
                 <svg className="w-5 h-5 text-yellow-400 mr-3 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
                   <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                 </svg>
-                <span className="text-blue-200 text-sm">+51 944 574 187 - Selección de Personal</span>
+                <span className="text-blue-200 text-sm">+51 888 777 666</span>
               </div>
               
               <div className="flex items-center justify-center md:justify-start">
@@ -69,7 +69,7 @@ const Footer = () => {
                   <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                   <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
                 </svg>
-                <span className="text-blue-200 text-sm">comercial@apolo300.com</span>
+                <span className="text-blue-200 text-sm">info@apolo300.com</span>
               </div>
               
               <div className="flex items-center justify-center md:justify-start">
@@ -77,7 +77,7 @@ const Footer = () => {
                   <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                   <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
                 </svg>
-                <span className="text-blue-200 text-sm">seleccion@apolo300.com</span>
+                <span className="text-blue-200 text-sm">ventas@apolo300.com</span>
               </div>
             </div>
           </div>
